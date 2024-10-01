444261c384016b34d2cd47f9f272cb4c584328f7
Dave O'Connor <git@mail.andvari.net>
2024-10-01 17:34:49 +0100

    working ls

--- database/database.go
+++ database/database.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os"
+	"reflect"
 	"strconv"
 )
 
@@ -16,6 +17,87 @@ type Database struct {
 	Mem      []MemorySlot `xml:"mem"`
 }
 
+// Each memory slot has track1,master,rhythm that are just k/v pairs.
+
+// Memory slot, one per <mem> tag.
+type MemorySlot struct {
+	XmlId  string   `xml:"id,attr"`
+	Name   SlotName `xml:"NAME"`
+	Master Master   `xml:"MASTER"`
+	Track1 Track1   `xml:"TRACK1"`
+	Rhythm Rhythm   `xml:"RHYTHM"`
+}
+
+// Slot names are represented in this funny way.
+type SlotName struct {
+	C01 int `xml:"C01"`
+	C02 int `xml:"C02"`
+	C03 int `xml:"C03"`
+	C04 int `xml:"C04"`
+	C05 int `xml:"C05"`
+	C06 int `xml:"C06"`
+	C07 int `xml:"C07"`
+	C08 int `xml:"C08"`
+	C09 int `xml:"C09"`
+	C10 int `xml:"C10"`
+	C11 int `xml:"C11"`
+	C12 int `xml:"C12"`
+}
+
+// Contents of the <MASTER> tag.
+type Master struct {
+	Tempo     int `xml:"Tempo"`
+	DubMode   int `xml:"DubMode"`
+	RecAction int `xml:"RecAction"`
+	AutoRec   int `xml:"AutoRec"`
+	FadeTime  int `xml:"FadeTime"`
+	Level     int `xml:"Level"`
+	LpMod     int `xml:"LpMod"`
+	LpLen     int `xml:"LpLen"`
+	TrkMod    int `xml:"TrkMod"`
+	Sync      int `xml:"Sync"`
+}
+
+type Track1 struct {
+	Rev      int `xml:"Rev"`
+	PlyLvl   int `xml:"PlyLvl"`
+	Pen      int `xml:"Pen"`
+	One      int `xml:"One"`
+	StrtMod  int `xml:"StrtMod"`
+	StpMod   int `xml:"StpMod"`
+	Measure  int `xml:"Measure"`
+	MeasMod  int `xml:"MeasMod"`
+	MeasLen  int `xml:"MeasLen"`
+	MeasBtLp int `xml:"MeasBtLp"`
+	RecTmp   int `xml:"RecTmp"`
+	WavStat  int `xml:"WavStat"`
+	WavLen   int `xml:"WavLen"`
+}
+
+type Rhythm struct {
+	Level           int `xml:"Level"`
+	Reverb          int `xml:"Reverb"`
+	Pattern         int `xml:"Pattern"`
+	Variation       int `xml:"Variation"`
+	VariationChange int `xml:"VariationChange"`
+	Kit             int `xml:"Kit"`
+	Beat            int `xml:"Beat"`
+	Fill            int `xml:"Fill"`
+	Part1           int `xml:"Part1"`
+	Part2           int `xml:"Part2"`
+	Part3           int `xml:"Part3"`
+	Part4           int `xml:"Part4"`
+	RecCount        int `xml:"RecCount"`
+	PlayCount       int `xml:"PlayCount"`
+	Start           int `xml:"Start"`
+	Stop            int `xml:"Stop"`
+	ToneLow         int `xml:"ToneLow"`
+	ToneHigh        int `xml:"ToneHigh"`
+	State           int `xml:"State"`
+}
+
+// Utility Functions for the above structs.
+
 // Get a memory slot by its number (zero-padded two-char string)
 func (d Database) GetMemorySlotByNumber(num string) *MemorySlot {
 	for _, mem := range d.Mem {
@@ -24,13 +106,6 @@ func (d Database) GetMemorySlotByNumber(num string) *MemorySlot {
 		}
 	}
 	return nil
-}
-
-// Memory slot, one per <mem> tag.
-type MemorySlot struct {
-	XmlId  string   `xml:"id,attr"`
-	Name   SlotName `xml:"NAME"`
-	Master Master   `xml:"MASTER"`
 }
 
 // The Memory Slot number, as a two-char padded string
@@ -48,24 +123,35 @@ func (m MemorySlot) Describe() string {
 	var ret string
 	ret += fmt.Sprintf("Mem: %v\n", m.Number())
 	ret += fmt.Sprintf("Name: %v\n", m.Name.String())
+	d, err := DescribeMemoryData(m.Master)
+	if err != nil {
+		return fmt.Sprintf("Error describing memory data: %v", err)
+	}
+	ret += d
+	d, err = DescribeMemoryData(m.Track1)
+	if err != nil {
+		return fmt.Sprintf("Error describing memory data: %v", err)
+	}
+	ret += d
+	d, err = DescribeMemoryData(m.Rhythm)
+	if err != nil {
+		return fmt.Sprintf("Error describing memory data: %v", err)
+	}
+	ret += d
 
 	return ret
 }
 
-// Slot names are represented in this funny way.
-type SlotName struct {
-	C01 int `xml:"C01"`
-	C02 int `xml:"C02"`
-	C03 int `xml:"C03"`
-	C04 int `xml:"C04"`
-	C05 int `xml:"C05"`
-	C06 int `xml:"C06"`
-	C07 int `xml:"C07"`
-	C08 int `xml:"C08"`
-	C09 int `xml:"C09"`
-	C10 int `xml:"C10"`
-	C11 int `xml:"C11"`
-	C12 int `xml:"C12"`
+func DescribeMemoryData(d interface{}) (string, error) {
+	var ret string
+	value := reflect.ValueOf(d)
+	if value.Type().Kind() != reflect.Struct {
+		return "", fmt.Errorf("expected struct, got %v", value.Type().Kind())
+	}
+	for i := 0; i < value.NumField(); i++ {
+		ret += fmt.Sprintf("%v: %v\n", value.Type().Field(i).Name, value.Field(i).Interface())
+	}
+	return ret, nil
 }
 
 // The string representation of the slot name.
@@ -106,19 +192,6 @@ func SlotNameFromString(str string) (*SlotName, error) {
 	ret.C11 = int(str[10])
 	ret.C12 = int(str[11])
 	return &ret, nil
-}
-
-type Master struct {
-	Tempo     int `xml:"Tempo"`
-	DubMode   int `xml:"DubMode"`
-	RecAction int `xml:"RecAction"`
-	AutoRec   int `xml:"AutoRec"`
-	FadeTime  int `xml:"FadeTime"`
-	Level     int `xml:"Level"`
-	LpMod     int `xml:"LpMod"`
-	LpLen     int `xml:"LpLen"`
-	TrkMod    int `xml:"TrkMod"`
-	Sync      int `xml:"Sync"`
 }
 
 func LoadMemoryFile(filename string) (*Database, error) {
